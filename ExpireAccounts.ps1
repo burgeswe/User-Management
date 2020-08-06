bd7548dadc9e6d8da4015ecb4e7976ff87b3cd61
Will Burgess <burgeswe@hotmail.com>
2020-08-06 16:02:19 -0500

    Add files via upload
    AD Powershell, Prompts user for username and sets their account to expire in "x" days

---
+++ ExpireAccounts.ps1
@@ -0,0 +1,12 @@
+﻿#requires -Modules ActiveDirectory
+
+# Ask for username input
+$user = Read-Host -Prompt 'Input the user name'
+
+# Variable to set account to expire in 'x' days
+$Days = -1
+
+# expiration date is today plus or minus the number of days
+$expirationDate = (Get-Date).AddDays($Days)
+
+Set-ADUser -Identity $user -AccountExpirationDate $expirationDate
